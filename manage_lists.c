6b8c83475ecd1aac437769cdc8d571c1e6917c64
Eric Muia <erick.muia.n@gmail.com>
2023-05-24 21:37:56 +0300

    Update manage_lists.c

--- manage_lists.c
+++ manage_lists.c
@@ -1,23 +1,3 @@
-
-Conversation opened. 1 read message.
-
-Skip to content
-Using Gmail with screen readers
-Enable desktop notifications for Gmail.
-   OK  No thanks
-3 of 668
-Shell
-Inbox
-
-Eric Muia <erick.muia.n@gmail.com>
-Attachments
-May 20, 2023, 4:33 PM (3 days ago)
-to me
-
-
-13
- Attachments
-  •  Scanned by Gmail
 #include "main.h"
 
 /**
@@ -172,5 +152,3 @@ void free_list(list_s **head_ptr)
 	}
 	*head_ptr = NULL;
 }
-manage_lists.c
-Displaying 0_string_functions.c.
