ab041e9d0c7f1151541223929278a3d39915d59b
Eric Muia <erick.muia.n@gmail.com>
2023-05-24 21:40:31 +0300

    Update split_string.c

--- split_string.c
+++ split_string.c
@@ -1,23 +1,3 @@
-
-Conversation opened. 1 read message.
-
-Skip to content
-Using Gmail with screen readers
-Enable desktop notifications for Gmail.
-   OK  No thanks
-3 of 668
-Shell
-Inbox
-
-Eric Muia <erick.muia.n@gmail.com>
-Attachments
-May 20, 2023, 4:33 PM (3 days ago)
-to me
-
-
-13
- Attachments
-  •  Scanned by Gmail
 #include "main.h"
 
 /**
@@ -127,5 +107,3 @@ char **strtow(char *str, char *d)
 	s[j] = NULL;
 	return (s);
 }
-split_string.c
-Displaying 0_string_functions.c.
